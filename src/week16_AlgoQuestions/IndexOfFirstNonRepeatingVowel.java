8e8e5ec34fd7d0fdaae81ee37560604d9725f19e
martinasenov <martin.manuilov.asenov@gmail.com>
2024-02-13 23:39:16 +0100

    week16_AlgoQuestions is added

---
+++ src/week16_AlgoQuestions/IndexOfFirstNonRepeatingVowel.java
@@ -0,0 +1,48 @@
+package week16_AlgoQuestions;
+
+public class IndexOfFirstNonRepeatingVowel {
+}
+
+
+
+
+/*
+
+
+
+Question-2.The Index of the first Non-repeating Vowel
+Given a string, write a method to find the index of the first non-repeating vowel (a, e, i, o, u) in it and return it.
+If it doesn't exist, return -1. Assume that string is not empty, and all the character of the String is lowercase.
+
+Try to solve with Map data structure like using a Map<Character, Integer>.
+
+Example-1:
+Input: “google”
+Output: 5
+Explanation: only e is non-repeated vowel, and its index is 5
+
+Example-2:
+Input: “responsible”
+Output: 4
+Explanation: e is repeated, "o" and "i" are not repeated, o is the first non-repeating vowel
+
+Example-3:
+Input: “mood”
+Output: -1
+Explanation: o is repeated, no non-repeating vowel.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
